2a36d53f225fa13a579ef531bc20874f6301d540
Adrianpardo2004 <iapardo@ucompensar.edu.co>
2025-10-15 15:03:07 -0500

    Update Empleado.js
    despues de

--- models/Empleado.js
+++ models/Empleado.js
@@ -4,11 +4,20 @@ const empleadoSchema = new mongoose.Schema({
   nro_documento: { type: String, required: true, unique: true },
   nombre: { type: String, required: true },
   apellido: { type: String, required: true },
-  edad: Number,
-  genero: String,
+  edad: {
+    type: Number,
+    required: true,
+    min: [18, "La edad mínima es 18 años"],
+    max: [100, "La edad máxima es 100 años"],
+  },
+  genero: {
+    type: String,
+    enum: ["Masculino", "Femenino", "Otro"],
+    required: true,
+  },
   cargo: String,
-  correo: { type: String, required: true, unique: true }, // <-- correo obligatorio
-  password: { type: String, required: true },            // <-- contraseña obligatoria
+  correo: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   nro_contacto: String,
   estado: { type: String, enum: ["activo", "retirado"], default: "activo" },
   observaciones: [String],
